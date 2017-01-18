0cb44d79ca846ab35589032d2ecac24e2337757f
Paweł Naworol <nowas89@gmail.com>
2017-01-18 13:01:55 +0100

    fixed display results.

--- javascript.js
+++ javascript.js
@@ -11,12 +11,17 @@ document.getElementById('convert-button').addEventListener('click', function() {
         hours.value = '';
         minutes.value = '';
         seconds.value = '';
-        result.innerHTML = 'wprowadŹ poprawne właściwości';
+        result.innerHTML = 'wprowadź poprawne wartości'
+
     } else {
+
         var decimalHours = (Number(hours.value) + (Number(minutes.value) / 60) + (Number(seconds.value) / 3600)).toFixed(2);
-        result.innerHTML = 'liczba godzin:' + decimalHours;
-        hours.value = '';
-        minutes.value = '';
-        seconds.value = '';
+        document.getElementById('result').innerHTML = 'liczba godzin łącznie:  ' + decimalHours  + ',    ';
+        document.getElementById('hoursDecimal').innerHTML = hours.value +'h,    ';
+        document.getElementById('minutesDecimal').innerHTML = minutes.value + 'min,     ';
+        document.getElementById('secondsDecimal').innerHTML = seconds.value +   'sec.';
+
+
     }
+
 }, false);
