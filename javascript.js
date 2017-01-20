41f0e3d7e9128706d54fb4bd1547138e30840def
Paweł Naworol <nowas89@gmail.com>
2017-01-20 11:32:44 +0100

    resolved problems with display results

--- javascript.js
+++ javascript.js
@@ -1,27 +1,43 @@
 var hours = document.getElementById('hours');
 var minutes = document.getElementById('minutes');
-var seconds = document.getElementById('second');
+var seconds = document.getElementById('seconds');
 var result = document.getElementById('result');
-
 
 document.getElementById('convert-button').addEventListener('click', function() {
 
-    if (isNaN(hours.value) || isNaN(minutes.value) || isNaN(second.value)) {
+    if (isNaN(hours.value) || isNaN(minutes.value) || isNaN(seconds.value) || hours.value === '' || minutes.value === '' || seconds.value === '') {
         console.log('nie jest numerem - wprowadz poprawne wartosci');
-        hours.value = '';
-        minutes.value = '';
-        seconds.value = '';
-        result.innerHTML = 'wprowadź poprawne wartości'
+        var results = {
+            hoursResult: '',
+            minutesResult: '',
+            secondsResult: '',
+            decimalResult: 'please . . .  enter the correct value'
+        }
+        displayResults(results);
 
     } else {
 
         var decimalHours = (Number(hours.value) + (Number(minutes.value) / 60) + (Number(seconds.value) / 3600)).toFixed(2);
-        document.getElementById('result').innerHTML = 'liczba godzin łącznie:  ' + decimalHours  + ',    ';
-        document.getElementById('hoursDecimal').innerHTML = hours.value +'h,    ';
-        document.getElementById('minutesDecimal').innerHTML = minutes.value + 'min,     ';
-        document.getElementById('secondsDecimal').innerHTML = seconds.value +   'sec.';
-
+        results = {
+            hoursResult: hours.value + 'h /',
+            minutesResult: minutes.value + 'min /',
+            secondsResult: seconds.value + 'sec',
+            decimalResult: 'total number of hours in decimal form:  ' + decimalHours
+        }
+        displayResults(results);
 
     }
-
 }, false);
+
+function displayResults(results) {
+
+
+    document.getElementById('result').innerHTML = results.decimalResult;
+    document.getElementById('hoursDecimal').innerHTML = results.hoursResult;
+    document.getElementById('minutesDecimal').innerHTML = results.minutesResult;
+    document.getElementById('secondsDecimal').innerHTML = results.secondsResult;
+    hours.value = '';
+    minutes.value = '';
+    seconds.value = '';
+
+}
